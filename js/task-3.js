0f8eca4e1e27d217059826843870531a8eb8c389
tolgaCETIK <tolgacetik@hotmail.com>
2025-05-27 21:54:44 +0200

    first commit

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+    const spaceArray = [];
+    for (const number of numbers) {
+    if (number > value) {
+        spaceArray.push(number);
+    }
+    }
+    return spaceArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
